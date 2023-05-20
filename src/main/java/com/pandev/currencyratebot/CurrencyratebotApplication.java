707e61bb5d6c8d4340d8cae484f350cc67f094c7
Roge55555 <roge55555@gmail.com>
2023-05-20 16:57:23 +0300

    initial commit

---
+++ src/main/java/com/pandev/currencyratebot/CurrencyratebotApplication.java
@@ -0,0 +1,13 @@
+package com.pandev.currencyratebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyratebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyratebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pandev/currencyratebot/CurrencyratebotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandev.currencyratebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyratebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
